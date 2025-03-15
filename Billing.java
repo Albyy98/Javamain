8f99d4c188db34eaa724c8abaff61aac2b6a3d3d
Akshayakku12 <akshayravi660@gmail.com>
2025-03-15 11:03:56 +0530

    Example Program

---
+++ Billing.java
@@ -0,0 +1,12 @@
+public class Billing{
+    public static void main(String []args){
+        int i;
+        int[] Customerid={100,101,102,103,104};
+        String[] Customername={"Albyy","Ponni","Karikalan","Biju","Mbappe"};
+        for(i=0;i<5;i++)
+        {
+            System.out.println(Customerid[i]);
+            System.out.println(Customername[i]);
+        }
+    }
+}
