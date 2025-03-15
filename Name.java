d6a32dc0f49099487b8d41640e022b6f87c26e30
Akshayakku12 <akshayravi660@gmail.com>
2025-03-15 12:04:05 +0530

    Example Program

---
+++ Name.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Name{
+    public static void main(String []args){
+        Scanner myobj=new Scanner(System.in);
+        System.out.println("Enter your Name:");
+        String name=myobj.nextLine();
+        System.out.println("The Name is:"+name);
+    }
+}
